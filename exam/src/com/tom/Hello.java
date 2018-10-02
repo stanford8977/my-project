af4a483aa9daf55a23b3fb1b4da5bb88d1772742
user <user@es302n04>
2018-10-02 11:32:17 +0800

    exam

--- exam/src/com/tom/Hello.java
+++ exam/src/com/tom/Hello.java
@@ -4,16 +4,20 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age = 18;
-	    float weight = 62f;
-	    float height = 175f;
-	    String firstname = ("Lin");
-	    String lastname = ("hao");
-	    System.out.println(age);
-	    System.out.println("weight");
-	    System.out.println("height");
-	    System.out.println("firstname");
-	    System.out.println("lastname");
+	 int age = 18;
+	 float weight = 62f;
+	 float height = 1.75f;
+	 String firstname =("Lin");
+	 String lastname = ("hao");
+	 System.out.println(age);
+	 System.out.println(weight);
+	 System.out.println(height);
+	 System.out.println(firstname);
+	 System.out.println(lastname);
+	 float bmi = weight / (height * height);
+	 System.out.println(bmi);
+	 age = age + 1;
+	 System.out.println(age);
 	}
 
 }
