2fd8ca227a41870f41f3160a5fd35b7b4013c063
user <user@es302n04>
2018-10-02 11:03:08 +0800

    second

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -4,16 +4,17 @@ public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int age = 18;
-		float weight = 62f;
-		float height = 175f;
-		String firstname = ("Lin");
-		String lastname = ("hao");
-	    System.out.println(age);
+        int age = 18;
+        float weight = 62f;
+        float height = 175f;
+        String firstname = ("Lin");
+        String lastname = ("hao");
+        System.out.println(age);
         System.out.println("weight");
         System.out.println("height");
-        System.out.println(firstname);
-        System.out.println(lastname);
+        System.out.println("firstname");
+        System.out.println("lastname");
+        	
 	}
 
 }
