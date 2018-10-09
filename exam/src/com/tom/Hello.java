e501b02af64fc42784f5747c23fe1e6ca700bc99
user <user@es302n04>
2018-10-09 12:01:11 +0800

    Constructor

--- exam/src/com/tom/Hello.java
+++ exam/src/com/tom/Hello.java
@@ -1,6 +1,6 @@
-package com.tom;
+  package com.tom;
 
-public class Hello {
+    public class Hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,13 +11,22 @@ public class Hello {
 	 String lastname = ("hao");
 	 System.out.println(age);
 	 System.out.println(weight);
-	 System.out.println(height);
-	 System.out.println(firstname);
-	 System.out.println(lastname);
+     System.out.println(height);
+     System.out.println(firstname);
+     System.out.println(lastname);
 	 float bmi = weight / (height * height);
 	 System.out.println(bmi);
 	 age = age + 1;
 	 System.out.println(age);
-	}
+	 Person p = new Person(62f,1.75f);
+//	 p.weight = 62f;
+//	 p.height = 1.75f;
+	 
+	 System.out.println(p.bmi());
+	 p.hello();
+//     Person Lin  = new Person();
+//     Person jack = null ;
+//     jack.hello();
+     }
 
 }
