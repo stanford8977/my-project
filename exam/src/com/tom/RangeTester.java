042357071fd0b93213d057d48b5ca51a3229d894
user <user@es302n04>
2018-10-23 12:03:12 +0800

    Member

--- exam/src/com/tom/RangeTester.java
+++ exam/src/com/tom/RangeTester.java
@@ -7,11 +7,11 @@ public class RangeTester {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
      
-	 System.out.println("Is number 2 locates on -3~5?(true/false)");
-     Scanner scanner = new Scanner(System.in);
-     String line = scanner.nextLine();
-     boolean number = line.equalsIgnoreCase("true");
-     System.out.println("number");
-	}
+	 System.out.println("Which number is between -3~5?(true/false)");
+	 Scanner scanner = new Scanner(System.in);
+     String  line = scanner.nextLine();
+     boolean range = line.equalsIgnoreCase("true&&false");
+     System.out.println(range);
+    }
 
 }
--- exam/src/com/tom/signup.java
+++ exam/src/com/tom/signup.java
@@ -9,11 +9,25 @@ public class signup {
         System.out.println("Are you 18?(y/N)");
         Scanner scanner = new Scanner(System.in);
         String line = scanner.nextLine();
-        
-        System.out.println("*".line."*");
+        System.out.println(line);
         boolean adult = line.equalsIgnoreCase("y");
-        System.out.println("adult");
+        System.out.println(adult);
+        if (adult){
+        	System.out.println("Your age?");
+        	int age = scanner.nextInt();
+        	scanner.nextLine();
+        	System.out.println("Your name");
+        	String name = scanner.nextLine();
+        	System.out.println("Your nick name");
+        	String nickname = scanner.nextLine();
+        	System.out.println(age+"\t"+name+"\t"+nickname+"\t");
         
+        }else{
+        	System.out.println("xxxxx 888888");
+        	
+        	
+        	
+        }
         
 	}
 
