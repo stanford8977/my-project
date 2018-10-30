7e3c6bfc1f3d887672d00ff09ce4addfee5c0e93
user <user@es302n04>
2018-10-30 12:05:18 +0800

    temp

---
+++ exam/src/com/tom/practice2.java
@@ -0,0 +1,18 @@
+package com.tom;
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       double n = (double)(3/5.0);
+       System.out.println(n);
+       float m = 18.168876f;
+       String name = "Stanford";
+       System.out.printf("Hi:%s,Your age:%.2f"+'\n',name , m);
+       
+       double c = 24.0;
+       System.out.println(c*(9/5.0)+32);
+       
+	}
+
+}
---
+++ exam/src/com/tom/temp.java
@@ -0,0 +1,30 @@
+package com.tom;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class temp {
+
+	public static void main(String[] args) {
+		
+         /*System.out.println("Please enter temperature(Celsius):");
+         Scanner scanner = new Scanner(System.in);
+         double c = scanner.nextDouble();
+         double f = c * (9/5.0)+32;
+         System.out.printf("%.1f c = %.1f f ",c,f);*/
+         
+		 int a = 2;
+         while(a>1){
+        	 
+         
+         System.out.println("handsomeboy");
+         a = a - 2;
+         }
+         
+         Random random = new Random();
+	}
+}
+         
+         
+         
+
